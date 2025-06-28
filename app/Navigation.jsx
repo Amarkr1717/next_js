42bc0197fec620b296432df6d16641e3dcfb667b
amargiri1717@gmail.com <amargiri1717@gmail.com>
2025-06-28 10:06:27 +0530

    navigation in next.js

---
+++ app/Navigation.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+const Navigation = () => {
+    return(
+       <header className=" grid grid-cols-2">
+        <div>Logo</div>
+        <nav>
+            <ul className="flex gap-4">
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/About">About</Link>
+                </li>
+                <li>
+                    <Link href="/services">Services</Link>
+                </li>
+                <li>
+                    <Link href="/Contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+       </header>
+    )
+}
+
+export default Navigation;
